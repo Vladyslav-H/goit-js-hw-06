30c93a543765fb5fafe6da6e6a0b6ffeb00b1fbb
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-20 16:48:23 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+const liElementsArray = ingredients.map((element, index) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredients[index];
+  return liEl;
+});
+ingredientsListEl.append(...liElementsArray);
