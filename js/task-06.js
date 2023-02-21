b78295a43196439082f1eafaecf0a6e734b60bcc
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-21 13:35:35 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("input");
+const validLength = +inputEl.getAttribute("data-length");
+
+inputEl.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  if (event.currentTarget.value.length === validLength) {
+    event.currentTarget.classList.add("valid");
+    event.currentTarget.classList.remove("invalid");
+  } else {
+    event.currentTarget.classList.remove("valid");
+    event.currentTarget.classList.add("invalid");
+  }
+}
