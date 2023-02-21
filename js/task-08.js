a4c25d834f2558ca928209aaff90d66b2f3f65dc
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-21 22:22:03 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+const buttonEL = document.querySelector("button");
+formEl.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const emailValue = event.currentTarget.elements.email.value;
+  const passwordValue = event.currentTarget.elements.password.value;
+  const formData = {
+    name: emailValue,
+    password: passwordValue,
+  };
+
+  if (emailValue.length === 0 || passwordValue.length === 0) {
+    alert(`All fields must have value !!!`);
+  } else {
+    console.log(formData);
+  }
+  event.target.reset();
+}
