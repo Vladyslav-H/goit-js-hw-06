1fc0c5ef9263287190980893294dc710a14db06c
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-22 09:24:17 +0200

    Fixed task-05,js

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,7 @@ const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-  spanEl.textContent = event.currentTarget.value;
+  event.currentTarget.value !== ""
+    ? (spanEl.textContent = event.currentTarget.value)
+    : (spanEl.textContent = "Anonymous");
 }
