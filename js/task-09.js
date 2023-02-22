68a79837ea259e90f79608f968cfe8e24a0566ec
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-22 10:39:05 +0200

    Fixed task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const bodyEl = document.querySelector("body");
+ 
+const bodyEl = document.body;
 const spanEl = document.querySelector("span.color");
 const buttonEl = document.querySelector("button");
 
 buttonEl.addEventListener("click", onChangeBodyColor);
 
 function onChangeBodyColor(event) {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  spanEl.textContent = `hex ${getRandomHexColor()}, ${
-    bodyEl.style.backgroundColor
-  }`;
+ 
+  spanEl.textContent =  bodyEl.style.backgroundColor = getRandomHexColor();
+  
 }
